6af627af93b96605fa5d9807f167ba11143d1079
Andrew Short <andrewjshort@gmail.com>
2010-12-17 14:51:53 +1100

    BUGFIX: Require livequery if the request for a tagfield was made via AJAX so the field initialisation works.

--- code/TagField.php
+++ code/TagField.php
@@ -102,6 +102,12 @@ class TagField extends TextField {
 	public function Field() {
 		Requirements::javascript(THIRDPARTY_DIR . "/jquery/jquery.js");
 		Requirements::javascript(SAPPHIRE_DIR . "/javascript/jquery_improvements.js");
+
+		// If the request was made via AJAX, we need livequery to init the field.
+		if (Director::is_ajax()) {
+			Requirements::javascript(THIRDPARTY_DIR . '/jquery-livequery/jquery.livequery.js');
+		}
+
 		Requirements::javascript("tagfield/thirdparty/jquery-tags/jquery.tags.js");
 		Requirements::javascript("tagfield/javascript/TagField.js");
 		Requirements::css("tagfield/css/TagField.css");
