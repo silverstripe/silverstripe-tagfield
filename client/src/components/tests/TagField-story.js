a2b1a827347779e375025bb05cd09a96deefab94
Sabina Talipova <sabina.talipova@silverstripe.com>
2023-06-12 15:35:04 +1200

    FIX Replace 4 space indent with 2 spaces

--- client/src/components/tests/TagField-story.js
+++ client/src/components/tests/TagField-story.js
@@ -2,40 +2,40 @@ import React from 'react';
 import TagField from 'components/TagField';
 
 export default {
-    title: 'TagField/TagField',
+  title: 'TagField/TagField',
 
-    decorators: [
-        (storyFn) => (
-            <div style={{ width: '250px' }} className="ss-tag-field">
-                {storyFn()}
-            </div>
-        ),
-    ],
+  decorators: [
+    (storyFn) => (
+      <div style={{ width: '250px' }} className="ss-tag-field">
+        {storyFn()}
+      </div>
+    ),
+  ],
 };
 
 export const SimpleExample = () => (
-    <TagField
-      name="test"
-      options={[
-        { Title: 'One', Value: 1 },
-        { Title: 'Two', Value: 2 },
-        { Title: 'Three', Value: 3 },
-        { Title: 'Four', Value: 4 },
-        { Title: 'Five', Value: 5 },
-      ]}
-    />
+  <TagField
+    name="test"
+    options={[
+      { Title: 'One', Value: 1 },
+      { Title: 'Two', Value: 2 },
+      { Title: 'Three', Value: 3 },
+      { Title: 'Four', Value: 4 },
+      { Title: 'Five', Value: 5 },
+    ]}
+  />
 );
 
 export const MultipleSelection = () => (
-    <TagField
-      name="test"
-      multi
-      options={[
-        { Title: 'One', Value: 1 },
-        { Title: 'Two', Value: 2 },
-        { Title: 'Three', Value: 3 },
-        { Title: 'Four', Value: 4 },
-        { Title: 'Five', Value: 5 },
-      ]}
-    />
+  <TagField
+    name="test"
+    multi
+    options={[
+      { Title: 'One', Value: 1 },
+      { Title: 'Two', Value: 2 },
+      { Title: 'Three', Value: 3 },
+      { Title: 'Four', Value: 4 },
+      { Title: 'Five', Value: 5 },
+    ]}
+  />
 );
