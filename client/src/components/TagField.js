3ab28d83c030d8280d2f37416b4d42b4737194bc
Steve Boyd <emteknetnz@gmail.com>
2025-11-06 15:30:22 +1300

    ENH Refactor TagField from class to functional component

--- client/src/components/TagField.js
+++ client/src/components/TagField.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Select from 'react-select';
 import AsyncSelect from 'react-select/async';
 import AsyncCreatableSelect from 'react-select/async-creatable';
@@ -11,115 +11,44 @@ import url from 'url';
 import debounce from 'debounce-promise';
 import PropTypes from 'prop-types';
 
-class TagField extends Component {
-  constructor(props) {
-    super(props);
+const TagField = ({
+  lazyLoad = false,
+  options,
+  creatable = false,
+  multi = false,
+  disabled = false,
+  labelKey = 'Title',
+  valueKey = 'Value',
+  SelectComponent = Select,
+  AsyncCreatableSelectComponent = AsyncCreatableSelect,
+  AsyncSelectComponent = AsyncSelect,
+  CreatableSelectComponent = CreatableSelect,
+  value: propValue,
+  onChange,
+  optionUrl,
+  ...passThroughAttributesFromProps
+}) => {
+  const selectComponentRef = useRef(null);
 
-    this.selectComponentRef = React.createRef();
+  const [hasChanges, setHasChanges] = useState(false);
+  const [stateValue, setStateValue] = useState(propValue);
 
-    this.state = {
-      initalState: props.value ? props.value : [],
-      hasChanges: false,
-    };
-
-    if (!this.isControlled()) {
-      this.state = {
-        ...this.state,
-        value: props.value,
-      };
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleOnBlur = this.handleOnBlur.bind(this);
-    this.isValidNewOption = this.isValidNewOption.bind(this);
-    this.getOptions = this.getOptions.bind(this);
-    this.fetchOptions = debounce(this.fetchOptions, 500);
-  }
-
-  componentDidUpdate(previousProps, previousState) {
-    if (previousState.hasChanges !== this.state.hasChanges) {
-      const element = this.selectComponentRef.current.inputRef;
-      const event = new Event('change', { bubbles: true });
-      element.dispatchEvent(event);
-    }
-  }
-
-  /**
-   * Get the options that should be shown to the user for this tagfield, optionally filtering by the
-   * given string input
-   *
-   * @param {string} input
-   * @return {Promise<Array<Object>>|Promise<{options: Array<Object>}>}
-   */
-  getOptions(input) {
-    const { lazyLoad, options } = this.props;
-
-    if (!lazyLoad) {
-      return Promise.resolve(options);
-    }
-
-    if (!input) {
-      return Promise.resolve([]);
-    }
-    return this.fetchOptions(input);
-  }
-
-  /**
-   * Handle a change, either calling the change handler provided (if controlled) or updating
-   * internal state of this component
-   *
-   * @param {string} value
-   */
-  handleChange(value) {
-    this.setState({
-      hasChanges: false
-    });
-
-    if (JSON.stringify(this.state.initalState) !== JSON.stringify(value)) {
-      this.setState({
-        hasChanges: true
-      });
-    }
-
-    if (this.isControlled()) {
-      this.props.onChange(value);
-      return;
-    }
-
-    this.setState({
-      value,
-    });
-  }
-
-  /**
-   * Determine if this input should be "controlled" or not. Controlled inputs should rely on their
-   * value coming from props and a change handler provided to update the state stored elsewhere.
-   * This is specifically the case for use with `redux-form`.
-   *
-   * @return {boolean}
-   */
-  isControlled() {
-    return typeof this.props.onChange === 'function';
-  }
-
-  /**
-   * Required to prevent TagField being cleared on blur
-   *
-   * @link https://github.com/JedWatson/react-select/issues/805
-   */
-  handleOnBlur() {}
+  useEffect(() => {
+    const element = selectComponentRef.current.inputRef;
+    const event = new Event('change', { bubbles: true });
+    element.dispatchEvent(event);
+  }, [hasChanges]);
 
   /**
    * Initiate a request to fetch options, optionally using the given string as a filter.
+   * The actual fetch is delayed by 500ms to avoid excessive requests while the user is typing.
    *
    * @param {string} input
    * @return {Promise<{options: Array<Object>}>}
    */
-  fetchOptions(input) {
-    const { optionUrl, labelKey, valueKey } = this.props;
+  const fetchOptions = debounce((input) => {
     const fetchURL = url.parse(optionUrl, true);
     fetchURL.query.term = input;
-
     return fetch(url.format(fetchURL), { credentials: 'same-origin' })
       .then((response) => response.json())
       .then((json) => json.items.map(
@@ -129,127 +58,167 @@ class TagField extends Component {
           Selected: item.Selected,
         })
       ));
-  }
+  }, 500);
+
+  /**
+   * Get the options that should be shown to the user for this tagfield, optionally filtering by the
+   * given string input
+   *
+   * @param {string} input
+   * @return {Promise<Array<Object>>|Promise<{options: Array<Object>}>}
+   */
+  const getOptions = (input) => {
+    if (!lazyLoad) {
+      return Promise.resolve(options);
+    }
+
+    if (!input) {
+      return Promise.resolve([]);
+    }
+    return fetchOptions(input);
+  };
+
+  /**
+   * Determine if this input should be "controlled" or not. Controlled inputs should rely on their
+   * value coming from props and a change handler provided to update the state stored elsewhere.
+   * This is specifically the case for use with `redux-form`.
+   *
+   * @return {boolean}
+   */
+  const isControlled = () => typeof onChange === 'function';
+
+  /**
+   * Handle a change, either calling the change handler provided (if controlled) or updating
+   * internal state of this component
+   *
+   * @param {string} newValue
+   */
+  const handleChange = (newValue) => {
+    setHasChanges(false);
+
+    if (JSON.stringify(propValue || []) !== JSON.stringify(newValue)) {
+      setHasChanges(true);
+    }
+
+    if (isControlled()) {
+      onChange(newValue);
+      return;
+    }
+
+    setStateValue(newValue);
+  };
+
+  /**
+   * Required to prevent TagField being cleared on blur
+   *
+   * @link https://github.com/JedWatson/react-select/issues/805
+   */
+  const handleOnBlur = () => {};
+
+  /**
+   * Check if a value is in an array of options already
+   * @param {string} checkValue
+   * @param {array} checkOptions
+   * @param {string} key
+   * @returns {boolean}
+   */
+  const valueInOptions = (checkValue, checkOptions, key) => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of checkOptions) {
+      if (checkValue === item[key]) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   /**
    * Check if a new option can be created based on a given input
    * @param {string} inputValue
-   * @param {array|object} value
+   * @param {array|object} checkValue
    * @param {array} currentOptions
    * @returns {boolean}
    */
-  isValidNewOption(inputValue, value, currentOptions) {
-    const { valueKey } = this.props;
-
+  const isValidNewOption = (inputValue, checkValue, currentOptions) => {
     // Don't allow empty options
     if (!inputValue) {
       return false;
     }
 
     // Don't repeat the currently selected option
-    if (Array.isArray(value)) {
-      if (this.valueInOptions(inputValue, value, valueKey)) {
+    if (Array.isArray(checkValue)) {
+      if (valueInOptions(inputValue, checkValue, valueKey)) {
         return false;
       }
-    } else if (inputValue === value[valueKey]) {
+    } else if (inputValue === checkValue[valueKey]) {
       return false;
     }
 
     // Don't repeat any existing option
-    return !this.valueInOptions(inputValue, currentOptions, valueKey);
+    return !valueInOptions(inputValue, currentOptions, valueKey);
+  };
+
+  const optionAttributes = lazyLoad
+    ? { loadOptions: getOptions }
+    : { options };
+
+  let DynamicSelect = SelectComponent;
+  if (lazyLoad && creatable) {
+    DynamicSelect = AsyncCreatableSelectComponent;
+  } else if (lazyLoad) {
+    DynamicSelect = AsyncSelectComponent;
+  } else if (creatable) {
+    DynamicSelect = CreatableSelectComponent;
   }
 
-  /**
-   * Check if a value is in an array of options already
-   * @param {string} value
-   * @param {array} options
-   * @param {string} valueKey
-   * @returns {boolean}
-   */
-  valueInOptions(value, options, valueKey) {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const item of options) {
-      if (value === item[valueKey]) {
-        return true;
+  // Update the value to passthrough with the kept state provided this component is not
+  // "controlled"
+  const passThroughAttributes = {
+    ...passThroughAttributesFromProps,
+    optionUrl,
+  };
+  if (!isControlled()) {
+    passThroughAttributes.value = stateValue;
+  }
+
+  // if this is a single select then we just need the first value
+  if (!multi && passThroughAttributes.value) {
+    if (Object.keys(passThroughAttributes.value).length > 0) {
+      const singleValue =
+        passThroughAttributes.value[
+          Object.keys(passThroughAttributes.value)[0]
+        ];
+
+      if (typeof singleValue === 'object') {
+        passThroughAttributes.value = singleValue;
       }
     }
-    return false;
   }
 
-  render() {
-    const {
-      lazyLoad,
-      options,
-      creatable,
-      multi,
-      disabled,
-      labelKey,
-      valueKey,
-      SelectComponent,
-      AsyncCreatableSelectComponent,
-      AsyncSelectComponent,
-      CreatableSelectComponent,
-      ...passThroughAttributes
-    } = this.props;
+  const changedClassName = hasChanges ? '' : 'no-change-track';
 
-    const optionAttributes = lazyLoad
-      ? { loadOptions: this.getOptions }
-      : { options };
-
-    let DynamicSelect = SelectComponent;
-    if (lazyLoad && creatable) {
-      DynamicSelect = AsyncCreatableSelectComponent;
-    } else if (lazyLoad) {
-      DynamicSelect = AsyncSelectComponent;
-    } else if (creatable) {
-      DynamicSelect = CreatableSelectComponent;
-    }
-
-    // Update the value to passthrough with the kept state provided this component is not
-    // "controlled"
-    if (!this.isControlled()) {
-      passThroughAttributes.value = this.state.value;
-    }
-
-    // if this is a single select then we just need the first value
-    if (!multi && passThroughAttributes.value) {
-      if (Object.keys(passThroughAttributes.value).length > 0) {
-        const value =
-          passThroughAttributes.value[
-            Object.keys(passThroughAttributes.value)[0]
-          ];
-
-        if (typeof value === 'object') {
-          passThroughAttributes.value = value;
-        }
-      }
-    }
-
-    const changedClassName = this.state.hasChanges ? '' : 'no-change-track';
-
-    return (
-      <EmotionCssCacheProvider>
-        <DynamicSelect
-          {...passThroughAttributes}
-          isMulti={multi}
-          isDisabled={disabled}
-          cacheOptions
-          onChange={this.handleChange}
-          onBlur={this.handleOnBlur}
-          {...optionAttributes}
-          getOptionLabel={(option) => option[labelKey]}
-          getOptionValue={(option) => option[valueKey]}
-          noOptionsMessage={({ inputValue }) => (inputValue ? i18n._t('TagField.NO_OPTIONS', 'No options') : i18n._t('TagField.TYPE_TO_SEARCH', 'Type to search'))}
-          isValidNewOption={this.isValidNewOption}
-          getNewOptionData={(inputValue, label) => ({ [labelKey]: label, [valueKey]: inputValue })}
-          classNamePrefix="ss-tag-field"
-          className={changedClassName}
-          ref={this.selectComponentRef}
-        />
-      </EmotionCssCacheProvider>
-    );
-  }
-}
+  return (
+    <EmotionCssCacheProvider>
+      <DynamicSelect
+        {...passThroughAttributes}
+        isMulti={multi}
+        isDisabled={disabled}
+        cacheOptions
+        onChange={handleChange}
+        onBlur={handleOnBlur}
+        {...optionAttributes}
+        getOptionLabel={(option) => option[labelKey]}
+        getOptionValue={(option) => option[valueKey]}
+        noOptionsMessage={({ inputValue }) => (inputValue ? i18n._t('TagField.NO_OPTIONS', 'No options') : i18n._t('TagField.TYPE_TO_SEARCH', 'Type to search'))}
+        isValidNewOption={isValidNewOption}
+        getNewOptionData={(inputValue, label) => ({ [labelKey]: label, [valueKey]: inputValue })}
+        classNamePrefix="ss-tag-field"
+        className={changedClassName}
+        ref={selectComponentRef}
+      />
+    </EmotionCssCacheProvider>
+  );
+};
 
 TagField.propTypes = {
   name: PropTypes.string.isRequired,
@@ -268,19 +237,6 @@ TagField.propTypes = {
   AsyncCreatableSelectComponent: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   AsyncSelectComponent: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   CreatableSelectComponent: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-};
-
-TagField.defaultProps = {
-  labelKey: 'Title',
-  valueKey: 'Value',
-  disabled: false,
-  lazyLoad: false,
-  creatable: false,
-  multi: false,
-  SelectComponent: Select,
-  AsyncCreatableSelectComponent: AsyncCreatableSelect,
-  AsyncSelectComponent: AsyncSelect,
-  CreatableSelectComponent: CreatableSelect,
 };
 
 export { TagField as Component };
--- client/src/components/tests/TagField-test.js
+++ client/src/components/tests/TagField-test.js
@@ -1,9 +1,76 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* global jest, test, describe, beforeEach, it, expect, setTimeout, document */
 
-import React from 'react';
+import React, { forwardRef, useImperativeHandle } from 'react';
 import { render } from '@testing-library/react';
 import { Component as TagField } from '../TagField';
+
+const MockSelect = forwardRef(({ className, onChange, onBlur, isMulti, isDisabled, value, options }, ref) => {
+  useImperativeHandle(ref, () => ({
+    inputRef: document.createElement('input'),
+  }));
+  const hasValue = value !== undefined && value !== null && (Array.isArray(value) ? value.length > 0 : true);
+  return (
+    <div
+      className={`test-dynamic test-select ${className || ''}`}
+      data-testid="select-component"
+      data-is-multi={String(isMulti)}
+      data-is-disabled={String(isDisabled)}
+      data-has-on-change={String(typeof onChange === 'function')}
+      data-has-on-blur={String(typeof onBlur === 'function')}
+      data-has-value={String(hasValue)}
+      data-has-options={String(!!options)}
+    />
+  );
+});
+
+const MockCreatableSelect = forwardRef(({ className, onChange, isMulti, isDisabled, isValidNewOption, getNewOptionData }, ref) => {
+  useImperativeHandle(ref, () => ({
+    inputRef: document.createElement('input'),
+  }));
+  return (
+    <div
+      className={`test-dynamic test-creatable-select ${className || ''}`}
+      data-testid="creatable-component"
+      data-is-multi={String(isMulti)}
+      data-is-disabled={String(isDisabled)}
+      data-has-on-change={String(typeof onChange === 'function')}
+      data-has-is-valid-new-option={String(typeof isValidNewOption === 'function')}
+      data-has-get-new-option-data={String(typeof getNewOptionData === 'function')}
+    />
+  );
+});
+
+const MockAsyncSelect = forwardRef(({ className, isMulti, isDisabled, loadOptions }, ref) => {
+  useImperativeHandle(ref, () => ({
+    inputRef: document.createElement('input'),
+  }));
+  return (
+    <div
+      className={`test-dynamic test-async-select ${className || ''}`}
+      data-testid="async-component"
+      data-is-multi={String(isMulti)}
+      data-is-disabled={String(isDisabled)}
+      data-has-load-options={String(typeof loadOptions === 'function')}
+    />
+  );
+});
+
+const MockAsyncCreatableSelect = forwardRef(({ className, isMulti, isDisabled, loadOptions, isValidNewOption }, ref) => {
+  useImperativeHandle(ref, () => ({
+    inputRef: document.createElement('input'),
+  }));
+  return (
+    <div
+      className={`test-dynamic test-async-creatable-select ${className || ''}`}
+      data-testid="async-creatable-component"
+      data-is-multi={String(isMulti)}
+      data-is-disabled={String(isDisabled)}
+      data-has-load-options={String(typeof loadOptions === 'function')}
+      data-has-is-valid-new-option={String(typeof isValidNewOption === 'function')}
+    />
+  );
+});
 
 function makeProps(obj = {}) {
   return {
@@ -13,10 +80,10 @@ function makeProps(obj = {}) {
     lazyLoad: false,
     creatable: false,
     multi: true,
-    SelectComponent: () => <div className="test-dynamic test-select" />,
-    CreatableSelectComponent: () => <div className="test-dynamic test-creatable-select" />,
-    AsyncSelectComponent: () => <div className="test-dynamic test-async-select" />,
-    AsyncCreatableSelectComponent: () => <div className="test-dynamic test-async-creatable-select" />,
+    SelectComponent: MockSelect,
+    CreatableSelectComponent: MockCreatableSelect,
+    AsyncSelectComponent: MockAsyncSelect,
+    AsyncCreatableSelectComponent: MockAsyncCreatableSelect,
     ...obj,
   };
 }
@@ -40,7 +107,7 @@ test('TagField should render a CreatableSelect with creatable option', () => {
   expect(container.querySelector('.test-creatable-select')).not.toBeNull();
 });
 
-test('Tagfiled should render an AsyncSelect with lazy load option', () => {
+test('Tagfield should render an AsyncSelect with lazy load option', () => {
   const { container } = render(
     <TagField {...makeProps({
       lazyLoad: true
@@ -51,7 +118,7 @@ test('Tagfiled should render an AsyncSelect with lazy load option', () => {
   expect(container.querySelector('.test-async-select')).not.toBeNull();
 });
 
-test('Tagfiled should render a AsyncCreatableSelect with lazy load and creatable options', () => {
+test('Tagfield should render a AsyncCreatableSelect with lazy load and creatable options', () => {
   const { container } = render(
     <TagField {...makeProps({
       lazyLoad: true,
@@ -61,4 +128,211 @@ test('Tagfiled should render a AsyncCreatableSelect with lazy load and creatable
   );
   expect(container.querySelectorAll('.test-dynamic')).toHaveLength(1);
   expect(container.querySelector('.test-async-creatable-select')).not.toBeNull();
+});
+
+test('TagField should support isMulti prop for multi-select functionality', () => {
+  const { container } = render(
+    <TagField {...makeProps({
+      multi: true
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.test-dynamic')).toHaveLength(1);
+});
+
+test('TagField should support single select mode with isMulti false', () => {
+  const { container } = render(
+    <TagField {...makeProps({
+      multi: false
+    })}
+    />
+  );
+  expect(container.querySelectorAll('.test-dynamic')).toHaveLength(1);
+});
+
+test('TagField should call onChange when value changes in controlled mode', () => {
+  const onChange = jest.fn();
+  const { container } = render(
+    <TagField {...makeProps({
+      onChange,
+      value: []
+    })}
+    />
+  );
+  const selectComponent = container.querySelector('[data-testid="select-component"]');
+  expect(selectComponent).not.toBeNull();
+  expect(selectComponent.getAttribute('data-has-on-change')).toBe('true');
+});
+
+test('TagField should track initial state for change detection', () => {
+  const testValue = [{ value: 'test1', label: 'Test 1' }];
+  const { container } = render(
+    <TagField {...makeProps({
+      value: testValue
+    })}
+    />
+  );
+  const selectComponent = container.querySelector('.test-dynamic');
+  expect(selectComponent.getAttribute('data-has-value')).toBe('true');
+});
+
+test('TagField should use custom labelKey and valueKey for option rendering', () => {
+  const { container } = render(
+    <TagField {...makeProps({
+      labelKey: 'customLabel',
+      valueKey: 'customValue'
+    })}
+    />
+  );
+  expect(container.querySelector('[data-testid="select-component"]')).not.toBeNull();
+});
+
+test('TagField should apply no-change-track class when hasChanges is false', () => {
+  const { container } = render(
+    <TagField {...makeProps()}/>
+  );
+  const selectComponent = container.querySelector('.test-dynamic');
+  expect(selectComponent.className).toContain('no-change-track');
+});
+
+test('TagField should pass static options to select component', () => {
+  const options = [
+    { label: 'Option 1', value: 'opt1' },
+    { label: 'Option 2', value: 'opt2' }
+  ];
+  const { container } = render(
+    <TagField {...makeProps({
+      options,
+      lazyLoad: false
+    })}
+    />
+  );
+  expect(container.querySelector('[data-has-options="true"]')).not.toBeNull();
+});
+
+test('TagField should pass isMulti prop correctly', () => {
+  const { container: multiContainer } = render(
+    <TagField {...makeProps({ multi: true })} />
+  );
+  expect(multiContainer.querySelector('[data-is-multi="true"]')).not.toBeNull();
+
+  const { container: singleContainer } = render(
+    <TagField {...makeProps({ multi: false })} />
+  );
+  expect(singleContainer.querySelector('[data-is-multi="false"]')).not.toBeNull();
+});
+
+test('TagField should pass isDisabled prop correctly', () => {
+  const { container: disabledContainer } = render(
+    <TagField {...makeProps({ disabled: true })} />
+  );
+  expect(disabledContainer.querySelector('[data-is-disabled="true"]')).not.toBeNull();
+
+  const { container: enabledContainer } = render(
+    <TagField {...makeProps({ disabled: false })} />
+  );
+  expect(enabledContainer.querySelector('[data-is-disabled="false"]')).not.toBeNull();
+});
+
+test('TagField should pass onChange callback to select component', () => {
+  const onChange = jest.fn();
+  const { container } = render(
+    <TagField {...makeProps({ onChange })} />
+  );
+  expect(container.querySelector('[data-has-on-change="true"]')).not.toBeNull();
+});
+
+test('TagField should pass onBlur handler to select component', () => {
+  const { container } = render(
+    <TagField {...makeProps()} />
+  );
+  expect(container.querySelector('[data-has-on-blur="true"]')).not.toBeNull();
+});
+
+test('TagField should pass value to select component', () => {
+  const testValue = [{ value: 'test1', label: 'Test 1' }];
+  const { container } = render(
+    <TagField {...makeProps({ value: testValue })} />
+  );
+  expect(container.querySelector('[data-has-value="true"]')).not.toBeNull();
+});
+
+test('TagField should pass creatable callbacks when creatable is true', () => {
+  const { container } = render(
+    <TagField {...makeProps({ creatable: true })} />
+  );
+  expect(container.querySelector('[data-has-is-valid-new-option="true"]')).not.toBeNull();
+  expect(container.querySelector('[data-has-get-new-option-data="true"]')).not.toBeNull();
+});
+
+test('TagField should pass loadOptions callback for lazy load', () => {
+  const { container } = render(
+    <TagField {...makeProps({ lazyLoad: true })} />
+  );
+  expect(container.querySelector('[data-has-load-options="true"]')).not.toBeNull();
+});
+
+test('TagField should not pass loadOptions for static options', () => {
+  const options = [{ label: 'Option 1', value: 'opt1' }];
+  const { container } = render(
+    <TagField {...makeProps({ lazyLoad: false, options })} />
+  );
+  expect(container.querySelector('[data-testid="select-component"]')).not.toBeNull();
+});
+
+test('TagField should select correct component based on lazyLoad and creatable props', () => {
+  const { container: regularContainer } = render(
+    <TagField {...makeProps({ lazyLoad: false, creatable: false })} />
+  );
+  expect(regularContainer.querySelector('[data-testid="select-component"]')).not.toBeNull();
+
+  const { container: creatableContainer } = render(
+    <TagField {...makeProps({ lazyLoad: false, creatable: true })} />
+  );
+  expect(creatableContainer.querySelector('[data-testid="creatable-component"]')).not.toBeNull();
+
+  const { container: asyncContainer } = render(
+    <TagField {...makeProps({ lazyLoad: true, creatable: false })} />
+  );
+  expect(asyncContainer.querySelector('[data-testid="async-component"]')).not.toBeNull();
+
+  const { container: asyncCreatableContainer } = render(
+    <TagField {...makeProps({ lazyLoad: true, creatable: true })} />
+  );
+  expect(asyncCreatableContainer.querySelector('[data-testid="async-creatable-component"]')).not.toBeNull();
+});
+
+test('TagField should apply no-change-track class when value equals initial state', () => {
+  const initialValue = [{ label: 'initial', value: 'init' }];
+  const { container } = render(
+    <TagField {...makeProps({ value: initialValue })} />
+  );
+  expect(container.querySelector('.no-change-track')).not.toBeNull();
+});
+
+test('TagField should render with custom pass-through attributes', () => {
+  const { container } = render(
+    <TagField {...makeProps({ 'data-custom': 'value' })} />
+  );
+  expect(container.querySelector('[data-testid="select-component"]')).not.toBeNull();
+});
+
+test('TagField should handle populated value arrays', () => {
+  const { container } = render(
+    <TagField {...makeProps({ value: [{ label: 'test', value: 'test' }] })} />
+  );
+  expect(container.querySelector('[data-has-value="true"]')).not.toBeNull();
+});
+
+test('TagField should distinguish between controlled and uncontrolled modes', () => {
+  const onChange = jest.fn();
+  const { container: controlledContainer } = render(
+    <TagField {...makeProps({ onChange })} />
+  );
+  expect(controlledContainer.querySelector('[data-has-on-change="true"]')).not.toBeNull();
+
+  const { container: uncontrolledContainer } = render(
+    <TagField {...makeProps()} />
+  );
+  expect(uncontrolledContainer.querySelector('[data-testid="select-component"]')).not.toBeNull();
 });
